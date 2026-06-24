8ca94031dfd1b19409cc62ff82154e6fccf6fd31
Polina <128475862+yakhnovets@users.noreply.github.com>
2026-06-24 12:50:58 +0300

    feat(profile): add screen profile ui

--- feature-profile/build.gradle.kts
+++ feature-profile/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -19,13 +20,34 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    buildFeatures {
+        buildConfig = true
+        compose = true
+
+    }
+
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
---
+++ feature-profile/src/main/java/com/powakaz/nesttrack/feature_profile/presentation/ProfileScreen.kt
@@ -0,0 +1,538 @@
+package com.powakaz.nesttrack.feature_profile.presentation
+
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Badge
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.powakaz.nesttrack.feature_profile.R
+import coil.compose.AsyncImage
+import org.jetbrains.annotations.Async
+
+
+private val shape20 = RoundedCornerShape(20.dp)
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+
+                    Text(
+                        text = stringResource(id = R.string.profile_title),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 20.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black,
+                        fontFamily = FontFamily.SansSerif
+                    )
+                }
+            )
+        }
+    ) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            ShowGeneralCard()
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = stringResource(id = R.string.your_details),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray,
+                fontFamily = FontFamily.SansSerif
+            )
+
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                ShowDateOfBirthCard()
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                EditAvatarCard()
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            ShowFamilyMemberCard()
+
+        }
+    }
+
+}
+
+@Composable
+fun ShowDateOfBirthCard() {
+    Column(
+        modifier = Modifier
+            .width(200.dp)
+            .height(220.dp)
+            .padding(horizontal = 16.dp)
+            .shadow(
+                elevation = 8.dp,
+                shape = shape20
+            )
+            .clip(shape20)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFFFD0E9),
+                        Color(0xFFFDF7FB)
+                    )
+                )
+            )
+            .border(
+                2.dp,
+                Color.White,
+                shape20
+            )
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFFDC7E5)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.cake),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier.size(40.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(id = R.string.date_of_birth),
+            modifier = Modifier
+                .fillMaxWidth(),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.DarkGray,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "15 марта 1995",
+            modifier = Modifier
+                .fillMaxWidth(),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFEE5E99),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        TextButton(
+            onClick = {},
+            modifier = Modifier
+                .size(width = 120.dp, height = 32.dp),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 6.dp
+            ),
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+
+        ) {
+
+            Icon(
+                painter = painterResource(id = R.drawable.cal),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier.size(12.dp)
+            )
+
+            Spacer(modifier = Modifier.width(2.dp))
+
+            Text(
+                text = stringResource(id = R.string.edit_date),
+                modifier = Modifier.offset(y = ((-1).dp)),
+                textAlign = TextAlign.Center,
+                fontSize = 8.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFEE5E99),
+                fontFamily = FontFamily.SansSerif,
+
+                )
+
+        }
+        Spacer(modifier = Modifier.height(2.dp))
+    }
+}
+
+@Composable
+fun EditAvatarCard() {
+    Column(
+        modifier = Modifier
+            .width(200.dp)
+            .height(220.dp)
+            .padding(horizontal = 16.dp)
+            .shadow(
+                elevation = 8.dp,
+                shape = shape20
+            )
+            .clip(shape20)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFE2D1FF),
+                        Color(0xFFF5F1FF)
+                    )
+                )
+            )
+            .border(
+                2.dp,
+                Color.White,
+                shape20
+            )
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFD2C6FC)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ph),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier.size(40.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(id = R.string.avatar),
+            modifier = Modifier
+                .fillMaxWidth(),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.DarkGray,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        TextButton(
+            onClick = {},
+            modifier = Modifier
+                .size(width = 120.dp, height = 32.dp),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 6.dp
+            ),
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+
+        ) {
+
+            Icon(
+                painter = painterResource(id = R.drawable.phh),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier.size(12.dp)
+            )
+            Spacer(modifier = Modifier.width(2.dp))
+
+            Text(
+                text = stringResource(id = R.string.edit_avatar),
+                modifier = Modifier.offset(y = ((-1).dp)),
+                textAlign = TextAlign.Center,
+                fontSize = 8.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF6C43FF),
+                fontFamily = FontFamily.SansSerif,
+
+                )
+
+        }
+        Spacer(modifier = Modifier.height(2.dp))
+    }
+}
+
+
+@Composable
+fun ShowGeneralCard() {
+
+    Spacer(modifier = Modifier.height(10.dp))
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .shadow(
+                elevation = 8.dp,
+                shape = shape20
+            )
+            .clip(shape20)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFE7ECFF),
+                        Color(0xFFF9EBFC)
+                    )
+                )
+            )
+            .border(
+                3.dp,
+                Color.White,
+                shape20
+            )
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(10.dp))
+
+        ShowDefaultAvatarCard()
+
+        Spacer(modifier = Modifier.height(14.dp))
+
+        Text(
+            text = "Полина",
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color.DarkGray,
+            fontFamily = FontFamily.SansSerif
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextButton(
+            onClick = {},
+            modifier = Modifier.size(width = 140.dp, height = 36.dp),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 6.dp
+            ),
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.pen),
+                contentDescription = null,
+                tint = Color(0xFF1669FF),
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Text(
+                text = stringResource(id = R.string.edit_name),
+                textAlign = TextAlign.Center,
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF1669FF),
+                fontFamily = FontFamily.SansSerif
+            )
+        }
+    }
+}
+
+@Composable
+fun ShowDefaultAvatarCard() {
+    Box(
+        modifier = Modifier
+            .size(140.dp),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.girl),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(CircleShape)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0xFFFDF0F7),
+                            Color(0xFFE59DD3)
+                        )
+                    )
+                    //0xFFEDF0FF - муж
+                )
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(CircleShape)
+                .border(
+                    3.dp,
+                    Color.White,
+                    CircleShape
+                )
+
+        )
+    }
+}
+
+@Composable
+fun ShowFamilyMemberCard() {
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(110.dp)
+            .padding(horizontal = 16.dp)
+            .shadow(
+                elevation = 8.dp,
+                shape = shape20
+            )
+            .clip(shape20)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFE5E5FC),
+                        Color(0xFFF7E7FF)
+                    )
+                )
+            )
+            .border(
+                width = 3.dp,
+                color = Color.White,
+                shape = shape20
+            ),
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Box(
+            modifier = Modifier
+                .size(46.dp)
+                .clip(CircleShape)
+                .background(Color(0xFFD2C6FC)),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.calk),
+                contentDescription = null,
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(30.dp)
+                    .padding(2.dp)
+
+            )
+        }
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.family_member),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray,
+                fontFamily = FontFamily.SansSerif
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Text(
+                text = "c 12 февраля 2024",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                fontSize = 12.sp,
+                maxLines = 2,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF797A81),
+                fontFamily = FontFamily.SansSerif
+            )
+
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Image(
+            painter = painterResource(id = R.drawable.house),
+            modifier = Modifier
+                .height(100.dp)
+                .offset(x = (-10).dp),
+            contentDescription = null,
+        )
+
+    }
+
+}
