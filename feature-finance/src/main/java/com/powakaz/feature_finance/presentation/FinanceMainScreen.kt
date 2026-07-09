e18837c40748f0a55b363e079976d1ea47cc29f9
wadim <112875224+WadimKaa@users.noreply.github.com>
2026-07-09 10:34:15 +0300

    feat(finance)/fin head finance

--- feature-finance/src/main/java/com/powakaz/feature_finance/presentation/FinanceMainScreen.kt
+++ feature-finance/src/main/java/com/powakaz/feature_finance/presentation/FinanceMainScreen.kt
@@ -1,12 +1,16 @@
 package com.powakaz.feature_finance.presentation
 
 import android.widget.ProgressBar
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -20,10 +24,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.powakaz.feature_finance.R
 
 @Composable
 fun FinanceMainScreenRoute() {
@@ -88,60 +94,93 @@ fun Head() {
     ) {
         Box(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(2.dp)
                 .clip(RoundedCornerShape(15.dp))
                 .background(
                     Brush.linearGradient(
                         listOf(
-                            Color(0XFFf0f5fe),
-                            Color(0xFFfaeffd)
+                            Color(0XFFf1f5fd),
+                            Color(0xFFfceefe)
                         )
                     )
                 )
         ) {
-            Column(modifier = Modifier.padding(start = 16.dp, top = 20.dp, bottom = 20.dp)) {
-                Text(
-                    text = "Общий баланс",
-                    color = Color(0XFF10173f),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
-                Text(
-                    text = "Br 2 000",
-                    color = Color(0XFF147afd),
-                    fontSize = 36.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier.padding(top = 4.dp)
-                )
-                HorizontalDivider(
-                    color = Color(0XFFe3eafc),
-                    modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
-                )
-                Text(
-                    text = "Недельный бюджет",
-                    color = Color(0XFF10173f),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
-                Row(modifier = Modifier.padding(top = 6.dp)) {
+            Row() {
+                Column(
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 20.dp, bottom = 20.dp)
+                        .fillMaxWidth(0.45f)
+                ) {
                     Text(
-                        text = "Br 150",
-                        color = Color(0XFF147afd),
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Normal
-                    )
-                    Text(
-                        text = "осталось",
-                        color = Color(0XFF69709b),
+                        text = "Общий баланс",
+                        color = Color(0XFF10173f),
                         fontSize = 13.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        modifier = Modifier
-                            .padding(start = 4.dp, bottom = 1.dp)
-                            .align(Alignment.Bottom)
+                        fontWeight = FontWeight.SemiBold
                     )
+                    Text(
+                        text = "Br 2 000",
+                        color = Color(0XFF147afd),
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                    HorizontalDivider(
+                        color = Color(0XFFe3eafc),
+                        modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
+                    )
+                    Text(
+                        text = "Недельный бюджет",
+                        color = Color(0XFF10173f),
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Row(modifier = Modifier.padding(top = 6.dp, bottom = 10.dp)) {
+                        Text(
+                            text = "Br 150",
+                            color = Color(0XFF147afd),
+                            fontSize = 17.sp,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Text(
+                            text = "осталось",
+                            color = Color(0XFF69709b),
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier
+                                .padding(start = 4.dp, bottom = 1.dp)
+                                .align(Alignment.Bottom)
+                        )
+                    }
+                    FinanceHeadProgressBar(0.3f)
                 }
-                LinearProgressIndicator(modifier = Modifier.padding(top = 6.dp))
+                Image(
+                    painter = painterResource(R.drawable.img_wallet),
+                    contentDescription = null,
+                    modifier = Modifier.align(
+                        Alignment.CenterVertically
+                    )
+                )
             }
         }
     }
+}
+
+@Composable
+fun FinanceHeadProgressBar(progress: Float) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(6.dp)
+            .background(color = Color(0XFFd9e0fb), shape = RoundedCornerShape(16.dp))
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(start = 1.dp)
+                .fillMaxHeight()
+                .fillMaxWidth(progress.coerceIn(0f, 1f))
+                .background(color = Color(0XFF0e6afb), shape = RoundedCornerShape(16.dp))
+        )
+    }
+
 }
