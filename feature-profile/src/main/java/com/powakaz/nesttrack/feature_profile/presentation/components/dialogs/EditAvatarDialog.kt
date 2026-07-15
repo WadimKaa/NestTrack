85e5f19d5ff93ea3c84529700791a9f407038ec5
Polina <128475862+yakhnovets@users.noreply.github.com>
2026-07-15 16:31:51 +0300

    feat(profile): fix avatar dialog

--- feature-profile/src/main/java/com/powakaz/nesttrack/feature_profile/presentation/components/dialogs/EditAvatarDialog.kt
+++ feature-profile/src/main/java/com/powakaz/nesttrack/feature_profile/presentation/components/dialogs/EditAvatarDialog.kt
@@ -2,6 +2,7 @@ package com.powakaz.nesttrack.feature_profile.presentation.components.dialogs
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,7 +19,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -82,7 +85,11 @@ fun EditAvatarDialogContent(
                     .fillMaxWidth()
                     .height(40.dp)
                     .padding(horizontal = 16.dp)
-                    .clickable(onClick = onTakePhoto),
+                    .clip(RoundedCornerShape(12.dp))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = ripple(color = Color(0xFFBB96FF)),
+                        onClick = onTakePhoto),
                 verticalAlignment = Alignment.CenterVertically
             )
             {
@@ -124,7 +131,11 @@ fun EditAvatarDialogContent(
                     .fillMaxWidth()
                     .height(40.dp)
                     .padding(horizontal = 16.dp)
-                    .clickable(onClick = onPickFromGallery),
+                    .clip(RoundedCornerShape(12.dp))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = ripple(color = Color(0xFFBB96FF)),
+                        onClick = onPickFromGallery),
                 verticalAlignment = Alignment.CenterVertically
             )
             {
@@ -166,7 +177,11 @@ fun EditAvatarDialogContent(
                     .fillMaxWidth()
                     .height(40.dp)
                     .padding(horizontal = 16.dp)
-                    .clickable(onClick = onDeletePhoto),
+                    .clip(RoundedCornerShape(12.dp))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = ripple(color = Color(0xFFBB96FF)),
+                        onClick = onDeletePhoto),
                 verticalAlignment = Alignment.CenterVertically
             )
             {
